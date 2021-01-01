73e67696b4bf4d2eda61b5fa0934cdbfb60be3f0
Imani Aza <imanicaza@gmail.com>
2020-12-31 18:45:32 -0600

    card template implemented

--- genki-chosa/src/components/genki1-component/lesson-list/LessonSection.js
+++ genki-chosa/src/components/genki1-component/lesson-list/LessonSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../../button-component/Button';
+import Cards from './Cards';
 
 import './LessonSection.css';
 
@@ -8,21 +9,21 @@ function LessonSection() {
 
 	return (
 		<div className="review-container">
-			<h2>Choose a Focus</h2>
 
-			<p>What area would you like to focus on?</p>
+			<Cards />
 
-			<div className="review-btns">
+
+
+
+			{/* <div className="review-btns">
 				<Link to="/vocabulary">
 					<Button className="btns" buttonStyle="btn--primary" button-size="btn--large">Vocabulary <i className="fab fa-buffer" /> </Button>
 				</Link>
 
-				<Link to="grammar">
+				<Link to="/grammar">
 					<Button className="btns" buttonStyle="btn--primary" button-size="btn--large">Grammar <i className="fas fa-quote-right" /> </Button>
 				</Link>
-
-			</div>
-
+			</div> */}
 		</div>
 	);
 }
--- genki-chosa/src/components/welcome-component/Welcome.js
+++ genki-chosa/src/components/welcome-component/Welcome.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import {Button} from '../button-component/Button';
-import {Link} from 'react-router-dom';
+import { Button } from '../button-component/Button';
+import { Link } from 'react-router-dom';
 
 import './Welcome.css';
 
 function Welcome() {
 
+
 	return (
 		<div className="welcome-container">
 			<h2>Genki Chōsa</h2>
 			<small>A review of concepts from げんき 3rd Edition series.</small>
-            <p>Choose the volume you want to see. </p>
-            
+			<p>Choose the volume you want to see. </p>
+
 			<div className="welcome-btns">
 				<Link to="/genki1">
-					<Button className="btns" buttonStyle="btn--primary" button-size="btn--large">GENKI 1 <i className="fas fa-book"/> </Button>
+					<Button className="btns" buttonStyle="btn--primary" button-size="btn--large">GENKI 1 <i className="fas fa-book" /> </Button>
 				</Link>
 
 				<Link to="/genki2">
-					<Button className="btns" buttonStyle="btn--outline" button-size="btn--large">GENKI 2 <i className="fas fa-book"/> </Button>
+					<Button className="btns" buttonStyle="btn--outline" button-size="btn--large">GENKI 2 <i className="fas fa-book" /> </Button>
 				</Link>
+				
 
 			</div>
-			
+
 		</div>
 	);
 }
