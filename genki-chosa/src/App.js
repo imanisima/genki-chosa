26ebece3b083acc4cc28135f02fb115d17d7cc5c
Imani Aza <imanicaza@gmail.com>
2020-12-31 21:38:55 -0600

    change home color to gradient

--- genki-chosa/src/App.js
+++ genki-chosa/src/App.js
@@ -29,6 +29,7 @@ function App() {
 
 				<Footer />
 			</Router>
+			
 		</>
 	);
 }
--- genki-chosa/src/components/contact-component/Contact.js
+++ genki-chosa/src/components/contact-component/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Button} from '../button-component/Button';
-import {Link} from 'react-router-dom';
+import { Button } from '../button-component/Button';
+import { Link } from 'react-router-dom';
 
 import './Contact.css';
 
@@ -10,14 +10,15 @@ function Contact() {
 		<div className="contact-container">
 			<h2>Need to get in touch?</h2>
 			<p>Found a bug or have a suggestion?</p>
-            
+
 			<div className="Contact-btns">
 				<Link to="/github-issues"
-                    target='_blank'>
-					<Button className="btns" buttonStyle="btn--primary" button-size="btn--large"><i className="fas fa-exclamation-triangle"/> SUBMIT AN ISSUE <i className="fas fa-exclamation-triangle"/> </Button>
+					target='_blank'>
+					<Button className="btns" buttonStyle="btn--primary" button-size="btn--large"><i className="fas fa-exclamation-triangle" /> SUBMIT AN ISSUE <i className="fas fa-exclamation-triangle" /> </Button>
 				</Link>
+
 			</div>
-			
+
 		</div>
 	);
 }
